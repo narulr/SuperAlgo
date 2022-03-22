b30a18f234b1d182e7d6e6bc64f043fe24a30824
Ravi Narula <ravinarula@Ravis-MacBook-Pro.local>
2022-03-22 18:31:56 +0530

    datareader

--- strlit.py
+++ strlit.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 import streamlit as st
 import pandas as pd
 import mplfinance as mpf
-from pandas_datareader import data as pdr
+#from pandas_datareader import data as pdr
 import numpy as np
 
 
@@ -80,9 +80,9 @@ with st.sidebar.form('settings_form'):
     ]
     chart_type = st.selectbox('Chart type', options=chart_types, index=chart_types.index('candle'))
 
-    mav1 = st.number_input('Mav 1', min_value=3, max_value=30, value=3, step=1)
-    mav2 = st.number_input('Mav 2', min_value=3, max_value=30, value=6, step=1)
-    mav3 = st.number_input('Mav 3', min_value=3, max_value=30, value=9, step=1)
+    mav1 = st.number_input('Mav 1', min_value=3, max_value=30, value=5, step=1)
+    mav2 = st.number_input('Mav 2', min_value=3, max_value=30, value=20, step=1)
+    mav3 = st.number_input('Mav 3', min_value=3, max_value=30, value=50, step=1)
 
     st.form_submit_button('Apply')
 
